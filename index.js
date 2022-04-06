9f76a86f15775e846f05f3465a2f19c6d2953b92
Christopher Marzulli <chriszulli15@gmail.com>
2022-04-06 14:11:19 -0400

    commit tree 32e644aaa5aa3a16845ad402685724aee3286603

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+    return string.toUpperCase()
+}
+function whisper(string) {
+    return string.toLowerCase()
+}
+function logShout(string) {
+    console.log("HELLO")
+}
+function logWhisper(string) {
+    console.log("hello")
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()){
+        return "I can\'t hear you!"}
+    if (string === string.toUpperCase()){
+        return "YES INDEED!"}
+    if (string === "Let's have dinner together!") {
+        return "I would love to!"
+    }     
+
+    }
